1ed49ce0f21e68782577a0256e933e1dcbb6af40
Ian <iewing085@gmail.com>
2022-02-11 09:30:58 -0500

    Create PizzaTranslator

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
-export default function App() {
+const Cat = (props) => {
+  const [isHungry, setIsHungry] = useState(true);
+const [timesPetted, setTimesPetted] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>
+        I am {props.name}, and I am {isHungry ? "hungry" : "not hungry"}
+      </Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disable={!isHungry}
+        title={isHungry ? "Pour me some milk, please!" : "Thank you"}
+      />
+            <Button
+        onPress={() => {
+          setTimesPetted(timesPetted + 1);
+        }}
+        title={"Pet me!"}
+      />
+      <Text>
+        I have been petted {timesPetted} times!
+      </Text>
     </View>
   );
+};
+
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
+  return (
+    <View style={styles.topContainer}>
+      <TextInput style={{height: 50}}
+      placeholder="Type here to translate!"
+      onChangeText={newText => setText(newText)}
+      defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text.split(' ').map((word) => word && '🍕').join(' ')}
+      </Text>
+    </View>
+  )
 }
 
+const Cafe = () => {
+  return (
+    <>
+      <Cat name="Cat1" />
+      <Cat name="Cat3" />
+    </>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  topContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  }
 });
+
+export default PizzaTranslator;
