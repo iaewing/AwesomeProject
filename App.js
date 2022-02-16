19a99618ce251c6c7e1ca701d5adbfab08c9cad4
Ian <i.ewing@vehikl.com>
2022-02-16 09:28:16 -0500

    Begin setup of a api request

--- App.js
+++ App.js
@@ -1,164 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
+  ActivityIndicator,
   Button,
   FlatList,
   Image,
-  View,
   Text,
   TextInput,
   SectionList,
   StyleSheet,
   ScrollView,
+  View,
 } from "react-native";
-
-const logo = {
-  uri: "https://reactnative.dev/img/tiny_logo.png",
-  width: 64,
-  height: 64,
-};
-
-const Cat = (props) => {
-  const [isHungry, setIsHungry] = useState(true);
-  const [timesPetted, setTimesPetted] = useState(0);
-  return (
-    <ScrollView>
-      <Text>
-        I am {props.name}, and I am {isHungry ? "hungry" : "not hungry"}
-      </Text>
-      <Button
-        onPress={() => {
-          setIsHungry(false);
-        }}
-        disable={!isHungry}
-        title={isHungry ? "Pour me some milk, please!" : "Thank you"}
-      />
-      <Button
-        onPress={() => {
-          setTimesPetted(timesPetted + 1);
-        }}
-        title={"Pet me!"}
-      />
-      <Text>I have been petted {timesPetted} times!</Text>
-    </ScrollView>
-  );
-};
-
-const PizzaTranslator = () => {
-  const [text, setText] = useState("");
-  return (
-    <View style={styles.topContainer}>
-      <TextInput
-        style={{ height: 50 }}
-        placeholder="Type here to translate!"
-        onChangeText={(newText) => setText(newText)}
-        defaultValue={text}
-      />
-      <Text style={{ padding: 10, fontSize: 42 }}>
-        {text
-          .split(" ")
-          .map((word) => word && "🍕")
-          .join(" ")}
-      </Text>
-    </View>
-  );
-};
-
-const Cafe = () => {
-  return (
-    <>
-      <Cat name="Cat1" />
-      <Cat name="Cat3" />
-    </>
-  );
-};
-
-const ScrollViewExample = () => {
-  return (
-    <ScrollView maximumZoomScale={5} minimumZoomScale={0.5}>
-      <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{ fontSize: 96 }}>If you like</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{ fontSize: 96 }}>Scrolling down</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{ fontSize: 96 }}>What's the best</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{ fontSize: 96 }}>Framework around?</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{ fontSize: 80 }}>React Native</Text>
-    </ScrollView>
-  );
-};
-
-const FlatListBasics = () => {
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={[
-          { key: "Cameron" },
-          { key: "Devin" },
-          { key: "Dan" },
-          { key: "Dominic" },
-          { key: "Jackson" },
-          { key: "James" },
-          { key: "Joel" },
-          { key: "John" },
-          { key: "Jillian" },
-          { key: "Jimmy" },
-          { key: "Julie" },
-          { key: "aCameron" },
-          { key: "aDevin" },
-          { key: "aDan" },
-          { key: "aDominic" },
-          { key: "aJackson" },
-          { key: "aJames" },
-          { key: "aJoel" },
-          { key: "aJohn" },
-          { key: "aJillian" },
-          { key: "aJimmy" },
-          { key: "aJulie" },
-        ]}
-        renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
-      />
-    </View>
-  );
-};
-
-const SectionListBasics = () => {
-  return (
-    <View style={styles.container}>
-      <SectionList
-      sections={[
-        {title: 'Pineapple', data: ['Devin', 'Dan', 'Dominic']},
-        {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy']},
-      ]}
-      renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-      renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-      keyExtractor={(item, index) => index}
-      />
-    </View>
-  )
-};
 
 const styles = StyleSheet.create({
   topContainer: {
@@ -188,4 +40,25 @@ const styles = StyleSheet.create({
   },
 });
 
-export default SectionListBasics;
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  
+};
